415ec474cef1247d0e70cb08c31d6b21fc0676c5
jshemas <jshemas@hotmail.com>
2014-11-14 16:49:20 -0500

    fixing tons of bugs

--- javascripts/main.js
+++ javascripts/main.js
@@ -17,7 +17,11 @@ textAdventureApp.controller('textAdventureCtrl', function ($scope, $http) {
 			adventureDescription: data.adventureDescription,
 			currentRound: 1,
 			currentPaths: [],
+			currentPath: 0,
+			currentPathDescription: '',
 			playerAlive: true,
+			winnngRound: data.winnngRound,
+			hasWon: false,
 			paths: data.paths
 		}
 		updateCurrentPaths();
@@ -25,9 +29,19 @@ textAdventureApp.controller('textAdventureCtrl', function ($scope, $http) {
 	$scope.pathClick = function (id) {
 		console.log("pathClick");
 		for (var i = 0; i <= $scope.adventure.paths.length - 1; i++) {
-			if($scope.adventure.paths[i].id === id) {
+			if ($scope.adventure.paths[i].id === id) {
+				$scope.adventure.currentPath = id;
+				$scope.adventure.currentPathDescription = $scope.adventure.paths[i].pathResult;
 				if ($scope.adventure.paths[i].death) {
 					$scope.adventure.playerAlive = false;
+				} else if ($scope.adventure.currentPath === $scope.adventure.winnngRound) {
+					$scope.adventure.hasWon = true;
+				} else if (!$scope.adventure.paths[i].goToNextRound) {
+					for (var j = 0; j <= $scope.adventure.currentPaths.length - 1; j++) {
+						if($scope.adventure.currentPaths[j].id == $scope.adventure.currentPath) {
+							$scope.adventure.currentPaths.splice(j, 1);
+						}
+					};
 				} else {
 					$scope.adventure.currentRound = $scope.adventure.currentRound + 1;
 					updateCurrentPaths();
@@ -39,6 +53,8 @@ textAdventureApp.controller('textAdventureCtrl', function ($scope, $http) {
 		console.log("startOver");
 		$scope.adventure.currentRound = 1;
 		$scope.adventure.playerAlive = true;
+		$scope.adventure.currentPath = 0;
+		$scope.adventure.currentPathDescription = '';
 		updateCurrentPaths();
 	}
 });
