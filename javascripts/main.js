55f12128120432757d4208c9ed94e71f933485dd
jshemas <jshemas@hotmail.com>
2014-11-14 10:34:47 -0500

    adding a restart button

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,17 @@
 var textAdventureApp = angular.module('textAdventureApp', []);
 
 textAdventureApp.controller('textAdventureCtrl', function ($scope, $http) {
+	function updateCurrentPaths() {
+		console.log("updateCurrentPaths");
+		$scope.adventure.currentPaths = [];
+		for (var i = 0; i <= $scope.adventure.paths.length - 1; i++) {
+			if($scope.adventure.paths[i].round === $scope.adventure.currentRound) {
+				$scope.adventure.currentPaths.push($scope.adventure.paths[i]);
+			}
+		};
+	}
 	$http.get('adventure.json').success(function(data) {
+		console.log("json get");
 		$scope.adventure = {
 			adventureName: data.adventureName,
 			adventureDescription: data.adventureDescription,
@@ -10,19 +20,22 @@ textAdventureApp.controller('textAdventureCtrl', function ($scope, $http) {
 			playerAlive: true,
 			paths: data.paths
 		}
+		updateCurrentPaths();
+	});
+	$scope.pathClick = function (id) {
+		console.log("pathClick");
 		for (var i = 0; i <= $scope.adventure.paths.length - 1; i++) {
-			if($scope.adventure.paths[i].round === $scope.adventure.currentRound) {
-				$scope.adventure.currentPaths.push($scope.adventure.paths[i]);
+			if($scope.adventure.paths[i].id === id) {
+				if ($scope.adventure.paths[i].death) {
+					$scope.adventure.playerAlive = false;
+				}
 			}
 		};
-		$scope.pathClick = function (id) {
-			for (var i = 0; i <= $scope.adventure.paths.length - 1; i++) {
-				if($scope.adventure.paths[i].id === id) {
-					if ($scope.adventure.paths[i].death) {
-						$scope.adventure.playerAlive = false;
-					}
-				}
-			};
-		}
-	});
+	}
+	$scope.startOver = function () {
+		console.log("startOver");
+		$scope.adventure.currentRound = 1;
+		$scope.adventure.playerAlive = true;
+		updateCurrentPaths();
+	}
 });
